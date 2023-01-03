8707b12f109f1bf240903b07b60461410f8625c0
Alen Kapitanker <alengamer76@gmail.com>
2023-01-03 21:03:01 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+
+    export let data : PageData
+</script>
+
+
+<div class='mt-2'>
+        <h1 class="text-white font-extrabold text-8xl uppercase">This dog Isn't penny!</h1>
+</div>
+<div class="mt-20 justify-center flex">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src={data.message}/>
+</div>
+<div class="mt-6">
+    <h3 class="text-white text-2xl font-semibold">Credits to Cappy (Aleph)#3287</h3>
+</div>
